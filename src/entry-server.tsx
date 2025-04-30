c469471eb5c49ecac49b8afc4419307c203b11c6
DMZ <nguyen.dmz@gmail.com>
2025-04-30 20:30:40 +0700

    refactor(helmet): update helmet usage and add template comments
    Update the usage of `react-helmet-async` by importing it as a package and destructuring `Helmet` and `HelmetProvider`. Add template comments in `index.html` for future head element management. These changes improve code maintainability and prepare for future enhancements.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,13 +1,17 @@
-
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import App from './App';
+import helmetPkg from 'react-helmet-async';
+const { Helmet, HelmetProvider } = helmetPkg;
 
 export function render(url: string) {
+  const helmetContext = {};
   const html = ReactDOMServer.renderToString(
-    <StaticRouter location={url}>
-      <App />
-    </StaticRouter>
+    <HelmetProvider context={helmetContext}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </HelmetProvider>
   );
   
   return html;
